10f0b55eeabcb7d920c97caa0a625712eeb182b4
Bhushan <bhushanshirsath@Bhushans-MacBook-Air.local>
2021-01-05 16:26:26 +0530

    chng

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-# Setup fgviimcimited
+# Setup done
 cred = credentials.Certificate("serviceAccountkey.json")
 firebase_admin.initialize_app(cred)
 
