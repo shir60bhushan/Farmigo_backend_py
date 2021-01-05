0c6da012c1f6594485201dee4cc42f6a07d96878
Bhushan <bhushanshirsath@Bhushans-MacBook-Air.local>
2021-01-05 17:58:58 +0530

    vv

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 def getMarketDetails_function(request):
     """Responds to any HTTP request.
     Args:
-        request (flask.Request): HTTP request object.
-    Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
     request_json = request.get_json()
     if request.args and 'message' in request.args:
