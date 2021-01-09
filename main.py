2c8a2dcdbb0a3a1b706efabb7076c3fe6a712744
Bhushan <bhushanshirsath@Bhushans-MacBook-Air.local>
2021-01-09 13:16:44 +0530

    jhjh

--- main.py
+++ main.py
@@ -20,8 +20,7 @@ def getMarketDetails(request):
 
         
   
-def pushToFireStoreData(request):
-    ##today = datetime.datetime.now()
+def pushToFireStoreData():
    
 ##qqUsijng add t00o add documents with auto generated keys
 db.collection('persons').add({'name':'John', 'age':88, 'address': "New York"})
@@ -30,4 +29,27 @@ db.collection('persons').add({'name':'Mark', 'age':40, 'address': "Paris"})
 db.collection('persons').add({'name':'Harry', 'age':40, 'address': "London"})
 db.collection('persons').add({'name':'Ron', 'age':40, 'address': "Mlan"})
 
+# Create a reference for the document before setting
+data = {
+    'name': 'Harry Pottery',
+    'address': 'USA'
+}
+
+# Add a new doc in collection 'persons' with ID 'HP'
+db.collection('persons').document('HP').set(data)
+
+# Merge new data with existing data for 'HP'
+data = {'employed':True}
+db.collection('persons').document('HP').set(data, merge=True)
+
+# Using document() to get an auto generated ID with set()
+data = {
+    'name': 'Iron Man',
+    'address': 'USA'
+
+}
+document_reference=db.collection('heroes').document()
+document_reference.set(data)
+# Adding subcollections
+document_reference.collection('movies').add({'name':'Avengers'})
 
