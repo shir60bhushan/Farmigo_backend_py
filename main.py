713c89aef3b3978c81c18beea785de5c4709b658
Bhushan <bhushanshirsath@Bhushans-MacBook-Air.local>
2021-01-09 13:40:34 +0530

    sdf

--- main.py
+++ main.py
@@ -22,12 +22,11 @@ def getMarketDetails(request):
   
 def pushToFireStoreData():
 
-# Using add to add documents with auto generated keys
-db.collection('persons').add({'name':'John', 'age':40, 'address': "New York"})
-db.collection('persons').add({'name':'Jane', 'age':50, 'address': "Los Angeles"})
-db.collection('persons').add({'name':'Mark', 'age':40, 'address': "Paris"})
-db.collection('persons').add({'name':'Harry', 'age':40, 'address': "London"})
-db.collection('persons').add({'name':'Ron', 'age':40, 'address': "Milan"})
+## Using add to add documents with auto generated keys
+    db.collection('persons').add({'name':'Jane','age':50, 'address': "Los Angeles"})
+    db.collection('persons').add({'name':'Mark','age':40, 'address': "Paris"})
+    db.collection('persons').add({'name':'Harry','age':40, 'address': "London"})
+    db.collection('persons').add({'name':'Ron','age':40, 'address': "Milan"})
 
 # Create a reference for the document before setting
 data = {
